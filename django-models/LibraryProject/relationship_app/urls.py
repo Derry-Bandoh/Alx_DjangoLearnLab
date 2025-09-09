6ccbb15f05626291c4b5cbb9afc29e29ef14dc3b
abderry <derrybandoh100@gmail.com>
2025-09-09 11:36:35 +0000

    Refactor URL patterns in urls.py to improve clarity and consistency

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,11 +4,12 @@ route to the newly created views. Make sure to link both the
 function-based and class-based views.
 """
 from django.urls import path 
-from .views import list_books, "libraryDetailView"
 from .import views
+from .views import LibraryDetailView
+
 
 
 urlpatterns = [
-    path('views/', views.list_books),
-    path('detail/', views.libraryDetailView.as_view()),
+    path('views/', views.list_books, name= 'list_book'),
+    path('detail/', views.libraryDetailView.as_view(), name= 'LibraryDetailView'),
     ]
