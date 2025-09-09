bab71394ab13b89adf42352bff494bea9a7d148f
abderry <derrybandoh100@gmail.com>
2025-09-09 11:26:14 +0000

    Refactor URL patterns and views; rename book_list to list_books and deatail_view to libraryDetailView for consistency

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,12 @@
+"""
+Edit relationship_app/urls.py to include URL patterns that 
+route to the newly created views. Make sure to link both the 
+function-based and class-based views.
+"""
 from django.urls import path 
 from .import views
 
 urlpatterns = [
-    path('views/', views.book_list),
-    path('detail/', views.deatail_view.as_view()),
+    path('views/', views.list_books),
+    path('detail/', views.libraryDetailView.as_view()),
     ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,13 +5,13 @@ from .models import Book
 from .models import Library
 
 
-def book_list(request):
+def list_books(request):
     """This view should render a simple text list of book titles and their authors."""
     books = Book.objects.all()
     context = {'book_list': books}
     return render(request,'relationship_app/list_books.html', context)
 
-class deatail_view(DetailView):
+class libraryDetailView(DetailView):
     """Create a class-based view in relationship_app/views.py that displays details 
     for a specific library, listing all books available in that library."""
     model = Library #This the model that this view will use
