0900e0f423935113240bd3060e79816c46833ed5
abderry <derrybandoh100@gmail.com>
2025-09-09 11:12:36 +0000

    Refactor detail_view to use DetailView class and update import statement

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 """Views for the relationship_app."""
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
@@ -11,10 +11,10 @@ def book_list(request):
     context = {'book_list': books}
     return render(request,'relationship_app/list_books.html', context)
 
-class deatail_view(ListView):
+class deatail_view(DetailView):
     """Create a class-based view in relationship_app/views.py that displays details 
     for a specific library, listing all books available in that library."""
-    model = Library
+    model = Library #This the model that this view will use
     template_name = 'relationship_app/library_detail.html'
    
 
