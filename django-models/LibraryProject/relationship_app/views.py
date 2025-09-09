924124b4227f883a55f8b258d98bd52530825ff2
abderry <derrybandoh100@gmail.com>
2025-09-09 11:05:09 +0000

    Refactor book list view and template; update rendering logic and restore list_books.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,14 @@ from .models import Book
 
 
 def book_list(request):
-    """View to list all books."""
+    """This view should render a simple text list of book titles and their authors."""
     books = Book.objects.all()
     context = {'book_list': books}
-    return render(request,'relationship_app/book_list.html', context)
+    return render(request,'relationship_app/list_books.html', context)
 
 class deatail_view(ListView):
+    """Create a class-based view in relationship_app/views.py that displays details 
+    for a specific library, listing all books available in that library."""
     
     template_name = 'relationship_app/library_detail.html'
    
