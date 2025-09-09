24a1ee584af973fe243d164df818ab91beb11a7b
abderry <derrybandoh100@gmail.com>
2025-09-09 14:09:12 +0000

    Add user authentication views and templates; implement registration, login, and logout functionality

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,12 +5,17 @@ function-based and class-based views.
 """
 from django.urls import path 
 from .import views
-from .views import LibraryDetailView
-from .views import list_books
+# from django.contrib.auth import views as auth_views
+# from .views import LibraryDetailView
+# from .views import list_books
 
 
 
 urlpatterns = [
     path('views/', views.list_books, name= 'list_book'),
     path('detail/', views.libraryDetailView.as_view(), name= 'LibraryDetailView'),
+    path('register/', views.user_registration.as_view(), name= 'register'),
+    path('login/',views.user_login.as_view(), name= 'login'),
+    path('logout/',views.user_logout.as_view(), name = 'logout'),
+    path('', views.home, name='home'),
     ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,12 @@
 """Views for the relationship_app."""
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 from .models import Book
 from .models import Library
 
@@ -11,10 +17,30 @@ def list_books(request):
     context = {'book_list': books}
     return render(request,'relationship_app/list_books.html', context)
 
+def home(request):
+    """A simple home view."""
+    return render(request, 'relationship_app/home.html')
+
 class libraryDetailView(DetailView):
     """Create a class-based view in relationship_app/views.py that displays details 
     for a specific library, listing all books available in that library."""
     model = Library #This the model that this view will use
     template_name = 'relationship_app/library_detail.html'
+
+class user_registration(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('user_login')
+
+class user_login(LoginView):
+    template_name = 'relationship_app/login.html'
+    redirect_authenticated_user = True
+
+class user_logout(LogoutView):
+    template_name = 'relationship_app/logout.html'
+    http_method_names = ['get', 'post']
+    # next_page = 'login'
+
    
 
