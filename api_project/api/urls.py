ecf2128262400e062673734913abe990d8bfb693
abderry <derrybandoh100@gmail.com>
2025-09-18 12:14:10 +0000

    Refactor URL routing and views to improve organization and clarity

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include 
 from rest_framework import routers
-from .views import BookList
+from .views import BookList, BookViewSet
 
-# router = routers.DefaultRouter()
-# router.register(r'books', BookList)
+router = routers.DefaultRouter()
+router.register(r'books_all', BookViewSet , basename='books_all')
 
 urlpatterns = [
-    # path('', include(router.urls)),
+    path('', include(router.urls)),
     path('books/', BookList.as_view(), name='book-list'),
 ]
