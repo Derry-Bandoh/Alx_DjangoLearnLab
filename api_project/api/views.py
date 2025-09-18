ef4cc3afd8ef9b5c2a19b30a7cb1cee623e0ba1c
abderry <derrybandoh100@gmail.com>
2025-09-18 12:01:47 +0000

    Refactor BookList view to use ListCreateAPIView directly for clarity

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 # from django.shortcuts import render
-from rest_framework import generics
+from rest_framework.generics import ListCreateAPIView
 from .models import Book
 from .serializers import BookSerializer 
 
 
-class BookList(generics.ListCreateAPIView):
+class BookList(ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
