dc3c45aa2c47f4fba3ed9c80a5ca01c601037ae8
abderry <derrybandoh100@gmail.com>
2025-09-18 12:06:11 +0000

    Refactor BookList view to inherit from ListAPIView for improved clarity

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,10 +1,10 @@
 # from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListAPIView
 from .models import Book
 from .serializers import BookSerializer 
 
 
-class BookList(ListCreateAPIView):
+class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
