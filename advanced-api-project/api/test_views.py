dcb387d821b3ee6a66661acbdf250ae6676c01dd
abderry <derrybandoh100@gmail.com>
2025-09-26 12:31:05 +0000

    Fourth test

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,6 +1,7 @@
-from django.test import 
+from django.test import TestCase
 from rest_framework.test  import APITestCase
 from .models import Book
+from rest_framework import status, response.data 
 
 class BookTestCase(TestCase):
 
