bdc89675e8dff09797fb9eb727792809aa578246
abderry <derrybandoh100@gmail.com>
2025-09-26 11:33:58 +0000

    Fix URL patterns for Book model to correct create, update, and delete routes

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,7 +7,7 @@ from .views import (
 urlpatterns = [
     path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
     path('books/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-detail'),
-    path('books/<int:pk>/create', BookListCreateAPIView.as_view(), name='book-create'),
-    path('books/<int:pk>/update', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-delete'),
+    path('books/create', BookListCreateAPIView.as_view(), name='book-create'),
+    path('books/update', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-update'),
+    path('books/delete', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-delete'),
 ]
