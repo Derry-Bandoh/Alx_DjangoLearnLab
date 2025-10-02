200a67ca5401394c8fd2a4672e8e3a5401a9031b
abderry <derrybandoh100@gmail.com>
2025-10-02 12:59:30 +0000

    Initial commit of Django blog project with basic structure, models, views, templates, and static files.

---
+++ django_blog/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.conf import settings 
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='blog_posts')
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
