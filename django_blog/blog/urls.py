86a84d6ee22d2993efd5fdcd21d9eabcdd625370
abderry <derrybandoh100@gmail.com>
2025-10-02 18:05:56 +0000

    Fix URL patterns for post delete and update views by adding missing trailing slashes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
     path('profile/', save, name = 'profile'),
     path('posts/',PostListView.as_view(), name ='post-list' ), 
     path('post/<int:pk>/',PostDetailView.as_view(), name = 'post-detail'),
-    path('post/<int:pk>/delete', PostDeleteView.as_view(), name ='post-delete'),
-    path('post/<int:pk>/update', PostUpdateView.as_view(), name = 'post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name ='post-delete'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name = 'post-update'),
     path('post/new/', PostCreateView.as_view(), name = 'post-create'),
 ]
